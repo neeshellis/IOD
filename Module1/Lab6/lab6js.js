320a4a30a2bd0075407f7c87b68211daba63c09d
neeshellis <neeshellis@gmail.com>
2025-07-14 19:13:58 +1200

    Lab6

--- Module1/Lab6/lab6js.js
+++ Module1/Lab6/lab6js.js
@@ -16,6 +16,37 @@ book.description = "A novel about elephants!";
 
 console.log(book);
 
-book.age = 2001;
+const bookarray = [
+  {
+    title: "Watership Down",
+    description: "A novel about a group of rabbits!",
+    author: "Fred Brown",
+    pages: 550,
+  },
+  {
+    title: "Titanic Down",
+    description: "A novel about Iceburgs!",
+    author: "Jane Brown",
+    pages: 551,
+  },
+  {
+    title: "The Spy who loves me",
+    description: "A novel about spys!",
+    author: "John Brown",
+    pages: 540,
+  },
+  {
+    title: "Cats",
+    description: "A novel about cats!",
+    author: "T S Elliot",
+    pages: 520,
+  },
+  {
+    title: "Watership Down",
+    description: "A novel about a group of rabbits!",
+    author: "Roger Brown",
+    pages: 500,
+  },
+];
 
-console.log(book);
+console.log(bookarray);
---
+++ Module1/Lab7/lab7js.js
@@ -0,0 +1,27 @@
+console.log("Hello World"); //string "Hello World"
+
+function sum(a, b) {
+  //string "Hello World"
+  console.log(a + b);
+}
+sum(5, 1);
+
+function subtract(c, d) {
+  console.log(c - d);
+}
+subtract(10, 5);
+
+function multiply(e, f) {
+  console.log(e * f);
+}
+multiply(7, 7);
+
+function division(g, h) {
+  console.log(g / h);
+}
+division(100, 10);
+
+function combine(name) {
+  console.log("Hello " + name);
+}
+combine("Neesh");
