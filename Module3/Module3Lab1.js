dc64018ff64263736abe83f6d43b6ea17bd8c127
neeshellis <neeshellis@gmail.com>
2025-07-24 15:09:23 +1200

    Mod3 Lab1 updates

--- Module3/Module3Lab1.js
+++ Module3/Module3Lab1.js
@@ -1,29 +1,30 @@
 //1
 // console.log("" + 1 + 0)  //10
-// console.log("" - 1 + 0)  //-1
+// console.log("" - 1 + 0)  //-1, a negative makes the string zero
 // console.log(true + false) //true
 // console.log(!true)   //false
-// console.log(6 / "3")  //2
+// console.log(6 / "3")  //2, a string in multiplication becomes the number
 // console.log("2" * "3")//6
 // console.log(4 + 5 + "px") //9px
 // console.log("$" + 4 + 5) //$45
 // console.log("4" - 2) //2
 // console.log("4px" - 2) //NaN
-// console.log(" -9 " + 5)//-95
+// console.log(" -9 " + 5)//-9 5
 // console.log(" -9 " - 5) //-14
 // console.log(null + 1) //1
 // console.log(undefined + 1) //NaN
 // console.log(undefined == null) // true
-// console.log( undefined === null) //false
-// console.log(" \t \n" - 2)  // \t \n - 2
+// console.log(undefined === null) //false
+// console.log(" \t \n" - 2)  // - 2
 
 //2
 // let three = "3"
 // let four = "4"
-// let thirty = "30"
-// //what is the value of the following expressions?
-// let addition = three + four //34 - incorrect
-// console.log( Number(3 + 4))
+// // let thirty = "30"
+
+// // //what is the value of the following expressions?
+// // let addition = three + four //34 - incorrect
+// console.log(Number(three) + Number(four)) - correct result of 7
 
 // let multiplication = three * four //12
 // console.log(three * four)
@@ -97,35 +98,35 @@
 // inigo.greeting(westley)
 // inigo.greeting(rugen)
 
-const westley = {
-  name: "Westley",
-  numFingers: 5,
-};
+// const westley = {
+//   name: "Westley",
+//   numFingers: 5,
+// };
 
-const rugen = {
-  name: "Count Rugen",
-  numFingers: 6,
-};
+// const rugen = {
+//   name: "Count Rugen",
+//   numFingers: 6,
+// };
 
-const inigo = {
-  firstName: "Inigo",
-  lastName: "Montoya",
-  greeting(person) {
-  let greeting = `Hello ${person.name}, my name is ${this.firstName} ${this.lastName} `;
-  console.log(greeting + this.getCatchPhrase(person));
-}, 
-// getCatchPhrase(person) {
-//     if (person.numFingers == 6) {
-//     return "You killed my father. Prepare to die.";
-//   }
-//     return "Nice to meet you.";
+// const inigo = {
+//   firstName: "Inigo",
+//   lastName: "Montoya",
+//   greeting(person) {
+//   let greeting = `Hello ${person.name}, my name is ${this.firstName} ${this.lastName} `;
+//   console.log(greeting + this.getCatchPhrase(person));
+// }, 
+// // getCatchPhrase(person) {
+// //     if (person.numFingers == 6) {
+// //     return "You killed my father. Prepare to die.";
+// //   }
+// //     return "Nice to meet you.";
+// // }
 // }
-}
-const getCatchPhrase = (person.numFingers != 6) => console.log("Nice to meet you.")
-const getCatchPhrase = (person.numFingers == 6) => console.log("You killed my father. Prepare to die.")
+// const getCatchPhrase = (person.numFingers != 6) => console.log("Nice to meet you.")
+// const getCatchPhrase = (person.numFingers == 6) => console.log("You killed my father. Prepare to die.")
 
-// 
-// console.log( 0 ? 'zero is true' : 'zero is false' ) // zero is false
+// // 
+// // console.log( 0 ? 'zero is true' : 'zero is false' ) // zero is false
 
-inigo.greeting(westley)
-inigo.greeting(rugen)
+// inigo.greeting(westley)
+// inigo.greeting(rugen)
