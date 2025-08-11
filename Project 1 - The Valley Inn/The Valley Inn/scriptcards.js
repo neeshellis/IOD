e0896ddd0e5fca5a13077b076e4b0b6fe1c118d0
neeshellis <neeshellis@gmail.com>
2025-08-12 10:59:29 +1200

    Project1 updates1

---
+++ Project 1 - The Valley Inn/The Valley Inn/scriptcards.js
@@ -0,0 +1,27 @@
+fetch("http://localhost:3000/pricingItems") // request data from this server// when it completes, access the JSON fromthe HTTP response sent by resolved promise
+  .then((response) => response.json()) // .json() also returns a promise
+  .then((json) => populate(json)) // log the returned JSON to the browser console
+  .catch((error) => console.error(error)); // if there was an error, log that too
+
+function addCard(title, description, image, price) {
+  // clone the template
+  const template = document
+    .getElementById("card-template")
+    .content.cloneNode(true);
+
+  // populate the template
+  template.querySelector(".card-title").innerText = title;
+  template.querySelector(".card-description").innerText = description;
+  template.querySelector(".card-image").src = image;
+  template.querySelector(".card-image").alt = title;
+  template.querySelector(".card-price").innerText = price;
+
+  // include the populated template into the page
+  document.querySelector("#card-list").appendChild(template);
+}
+
+function populate(json) {
+  json.forEach((item) => {
+    addCard(item.title, item.description, item.image, `$ ${item.price}`);
+  });
+}
